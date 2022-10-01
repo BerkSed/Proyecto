d1b74ee391ad709c1de5c7273cdcb5b8ffd6f467
Victor Sedeño <114040462+BerkSed@users.noreply.github.com>
2022-09-30 21:30:09 -0500

    Add files via upload

---
+++ CV personal/index.php
@@ -0,0 +1,417 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>CV Personal</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+	<script src="https://kit.fontawesome.com/dd5e6670f4.js" crossorigin="anonymous"></script>
+
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+	link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+	<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>.
+
+</head>
+<body>
+
+	<style>
+		body{
+			background-color: #E6F9FF;
+		}
+
+
+	</style>
+
+	<div class="container-fluid">
+		<div class="card bg-dark text-white">
+			<img src="img/img.png" class="card-img" alt="...">
+			<div class="card-img-overlay">
+				<h1 class="card-title display-1 text-center border border-3">
+					Hola, soy Victor Abraham Sedeño González 
+				</h1>
+
+				<br>
+
+				<p class="card-text text-center border border-3" style="font-size: 25px">
+					Desarrollador Web y Técnico en Tecnlogias de la Información y Comunicación
+				</p>
+
+			</div>
+		</div>
+
+		<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #051334; border-radius: 2px; " >
+			<div class="container-fluid">
+				<a class="navbar-brand" href="#">CV Profesional</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNav">
+					<ul class="navbar-nav">
+						<li class="nav-item">
+							<a class="nav-link" href="#Sobre mi">Sobre mi</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#estudios">Estudios</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#Portafolio">Portafolio</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#Intereses">Intereses</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#contacto">Contacto</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</div>
+
+
+	<div class="container">
+
+		<br>
+
+		<div class="row">
+			<div class="col-12">
+				<h1 class="display-6 text-center" id="Sobre mi"><i>Sobre mi</i></h1>
+				<hr>
+			</div>
+		</div>	
+
+		<br>
+
+		<div class="container-fluid">
+			<div class="row" >
+				<div class="col-5">
+					<div class="card" style="border-radius:10px">
+						<div class="card-body ">
+							<h4 class="card-title text-center display-6">¿Quién soy?</h4>
+						</div>
+						<img src="img/foto.jpg" class="card-img-bottom" alt="...">
+						<p class="card-text text-center" style="font-size: 25px;">Curriculum Vitae</p>
+					</div>
+				</div>
+
+				<div class="col-4">
+					
+					<p class="lh-base" style="text-align: justify; border: 1px solid #5CC23A; padding: 10px; border-radius: 10px;background-color: white;">Me prepare en la carrera de Tecnologias de la Información y Comunicación, una de mis metas es tener ese sentido de pertencia a la cual poder adaptarme y seguir aprendiendo, puede desempeñarme como desarrillador front-end y back-end, ademas de que cuento con conocimientos basicos en Diseño Digital. También tengo conocimientos en HTML5, CSS3, SQL y JavaScript.</p>
+
+					<p class="lh-base" style="text-align: justify; border: 1px solid #5CC23A; padding: 10px;border-radius: 10px; background-color: white;"> Me considero una persona con facilidad de aprendizaje, capaz de trabajar en equipo y compartir opiniones con mis compañeros de trabajo, desarrollle una dedicacion y compromiso con los proyectos que realizo, además de una busqueda constante de conocimiento sobre mi carrera y estar siempre actulizado a los tiempos y a las nuevas tecnologias que se presenten.</p>
+				</div>
+
+				<div class="col-3">
+					<div class="card" style="border-color: #D55F3E; border-radius:15px">
+						<div class="card-body">
+							<p class="card-text">Si tienes palabras más fuertes que el silencio, habla. Si no las tienes, entonces guarda silencio</p>
+							<h6 class="card-subtitle mb-2 text-muted">Eurípides</h6>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-12">
+				<h1 class="display-6 text-center" id="estudios"><i>Estudios</i></h1>
+				<hr>
+			</div>
+		</div>
+
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-4">
+					<div class="card" style="width: 18rem;">
+						<div class="card-header" style="background-color:#055FB5; color: white;">
+							Secundaria
+						</div>
+						<ul class="list-group list-group-flush">
+							<li class="list-group-item">Escuela Secundaria Tecnica 2º Francisco I. Madero</li>
+						</ul>
+
+						<div class="card-header" style="background-color:#5CC23A; color: white;">
+							Bachiller
+						</div>
+						<ul class="list-group list-group-flush">
+							<li class="list-group-item">Cobaep Plantel 2</li>
+							<li class="list-group-item">Conocimientos báscios en HTML</li>
+						</ul>
+
+						<div class="card-header" style="background-color:#D55F3E; color: white;">
+							TSU
+						</div>
+						<ul class="list-group list-group-flush">
+							<li class="list-group-item">Tecnologias de la Información y Comunicación</li>
+						</ul>
+					</div>
+				</div>
+
+				<div class="col-4">
+					<div class="card" style="width: 18rem;">
+						<div class="card-header" style="background-color:#8C3DC9; color: white;">
+							Conocimientos Básicos
+						</div>
+						<ul class="list-group list-group-flush">
+							<li class="list-group-item">Java</li>
+							<li class="list-group-item">HTML5</li>
+							<li class="list-group-item">CSS3</li>
+							<li class="list-group-item">SQL</li>
+							<li class="list-group-item">GIMP</li>
+							<li class="list-group-item">Adobe Illustrator</li>
+						</ul>
+					</div>
+				</div>
+
+				<div class="col-4">
+					<div class="card" style="width: 18rem;">
+
+						<div class="card-header" style="background-color:#DBA73F; color: white;">
+							Cursos
+						</div>
+						<ul class="list-group list-group-flush">
+							<li class="list-group-item">Cisco: NDG Linux Unhatched</li>
+							<li class="list-group-item">UDEMY: Curso de HTML5 y CSS3</li>
+						</ul>
+
+						<div class="card-header" style="background-color:#DBA73F; color: white;">
+							Habilidades
+						</div>
+						<ul class="list-group list-group-flush">
+							<li class="list-group-item">Basquetbol</li>
+							<li class="list-group-item">Futbol Americano</li>
+						</ul>
+					</div>
+				</div>
+
+			<div class="row">
+				<div class="col-12">
+					<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+						<div class="carousel-indicators">
+							<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+							<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+							<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+							<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+							<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
+							<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="5" aria-label="Slide 6"></button>
+							<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="6" aria-label="Slide 7"></button>
+						</div>
+						<div class="carousel-inner">
+							<div class="carousel-item active">
+								<img src="img/captura.png" class="d-block w-100" alt="...">
+							</div>
+							<div class="carousel-item">
+								<img src="img/certi.png" class="d-block w-100" alt="...">
+							</div>
+							<div class="carousel-item">
+								<img src="img/logo 4.png" class="d-block w-100" alt="...">
+							</div>
+							<div class="carousel-item">
+								<img src="img/gimp.jpg" class="d-block w-100" alt="...">
+							</div>
+							<div class="carousel-item">
+								<img src="img/codigo.png" class="d-block w-100" alt="...">
+							</div>
+							<div class="carousel-item">
+								<img src="img/unif.jpeg" class="d-block w-100" alt="...">
+							</div>
+							<div class="carousel-item">
+								<img src="img/tic.jpg" class="d-block w-100" alt="...">
+							</div>
+						</div>
+						<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+							<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+							<span class="visually-hidden">Previous</span>
+						</button>
+						<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+							<span class="carousel-control-next-icon" aria-hidden="true"></span>
+							<span class="visually-hidden">Next</span>
+						</button>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+
+		<div class="row">
+			<div class="col-12">
+				<br>
+				<h1 class="display-6 text-center" id="Portafolio"><i>Portafolio</i></h1>
+				<hr>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-6">
+
+				<h5 class="display-6 text-center" id="Portafolio" style="color: #D55F3E;"><i>Primer Cuatrimestre</i></h5>
+
+				<div class="card mb-3" style="max-width: 540px; ">
+					<div class="row g-0">
+						<div class="col-md-4">
+							<img src="img/java.png" class="img-fluid rounded-start" alt="...">
+						</div>
+						<div class="col-md-8">
+							<div class="card-body">
+								<p class="card-text">Concetos báscos de Java</p>
+								<p class="card-text"><small class="text-muted">Todo sobre variables, operadores, ciclos de repetición, además, aplique estos conocimientos en diferentes problemas que necesitaban aplicar estos conceptos</small></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-6">
+
+				<h5 class="display-6 text-center" id="Portafolio" style="color: #055FB5"><i>Segundo Cuatrimestre</i></h5>
+
+				<div class="card mb-3" style="max-width: 540px;">
+					<div class="row g-0">
+						<div class="col-md-4">
+							<img src="img/ilu.png" class="img-fluid rounded-start" alt="...">
+						</div>
+						<div class="col-md-8">
+							<div class="card-body">
+								<p class="card-text">Herramientas de Illustraitor</p>
+								<p class="card-text"><small class="text-muted">Conceptos básicos de Illustraitor, codigos de colores, creación de logos, infografias, tripticos, anuncios publicitarios, etc.</small></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-12">
+					<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+						<div class="carousel-inner">
+							<div class="carousel-item active">
+								<img src="img/foto1.jpg" class="d-block w-100" alt="...">
+							</div>
+							<div class="carousel-item">
+								<img src="img/replit.png" class="d-block w-100" alt="...">
+							</div>
+							<div class="carousel-item">
+								<img src="img/platzi.png" class="d-block w-100" alt="...">
+							</div>
+							<div class="carousel-item">
+								<img src="img/ilu1.png" class="d-block w-100" alt="...">
+							</div>
+						</div>
+						<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+							<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+							<span class="visually-hidden">Previous</span>
+						</button>
+						<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+							<span class="carousel-control-next-icon" aria-hidden="true"></span>
+							<span class="visually-hidden">Next</span>
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-12">
+				<br>
+				<h1 class="display-6 text-center" id="Intereses"><i>Intereses</i></h1>
+				<hr>
+			</div>
+		</div>
+
+		<div class="container-fluid text-center">
+			<div class="row">
+				<div class="col-4">
+					<div class="card" style="width: 18rem; border-color: orange; border-radius: 15px;">
+						<div class="card-body">
+							<h5 class="card-title" style="color:orange"><span class="material-symbols-outlined">
+								sports_basketball
+							</span></h5>
+							<h6 class="card-subtitle mb-2 text-muted">Basquetbol</h6>
+							<p class="card-text">Desde muy pequeño comence a practicar deporte, el basquetbol a sido muy importante para mi vida y creo que me ayudado a desarrollarme mejor</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-4">
+					<div class="card" style="width: 18rem;border-color: #23D5CC; border-radius: 15px;">
+						<div class="card-body">
+							<h5 class="card-title" style="color:#23D5CC"><span class="material-symbols-outlined">
+								brush
+							</span></h5>
+							<h6 class="card-subtitle mb-2 text-muted">Dibujar</h6>
+							<p class="card-text">Dibujo como un pasatiempo, eso hizo que me interesara el diseño y el gusto por los colores</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-4">
+					<div class="card" style="width: 18rem;border-color: #D52359; border-radius: 15px;">
+						<div class="card-body">
+							<h5 class="card-title" style="color: #D52359;"><span class="material-symbols-outlined">
+								videogame_asset
+							</span></h5>
+							<h6 class="card-subtitle mb-2 text-muted">Videojuegos</h6>
+							<p class="card-text">Un gusto muy común y creo que si estoy en este carrera es por que me intereso el como unas palabras pueden darle vida a algo tan complejo.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<br>
+
+	<div class="container-fluid" style="background-color:#051334">
+		<div class="row">
+			<div class="col-12">
+				<br>
+				<h1 class="display-6 text-center" id="contacto" style="color: white;"><i>Informacíon de contacto</i></h1>
+				<hr style="color: white;">
+			</div>
+
+			<div class="row">
+				<div class="col-6">
+					<h1 class="display-6 text-center" id="contacto" style="color: white;"><i>Buscame en:</i>
+						<br>
+						
+						<a href="https://www.facebook.com/gael.sedenogonzalez/"><ion-icon name="logo-facebook"></ion-icon></a>
+						<br>
+					</h1>
+				</div>
+
+				<div class="col-6 text-center" style="color:WHITE">
+					<h1 class="display-6 text-center" id="contacto" style="color: white;"><i>Contactame</i></h1>
+			
+					<span class="material-symbols-outlined">call</span> 
+					 221-324-7622
+					<hr>
+
+					<span class="material-symbols-outlined">
+						mail
+					</span> vicsedglez818@gmail.com
+					<hr>
+
+					<span class="material-symbols-outlined">
+						pin_drop
+					</span> San Juan Cuautlancingo Puebla
+					<hr>
+							
+							
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+
+
+	<script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+
+	
+</body>
+</html>
